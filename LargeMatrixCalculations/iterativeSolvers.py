84237a1148d93a9d3590934cc06d9406a317f259
iPupkin <blakeyvd@gmail.com>
2022-10-25 20:42:23 -0600

    added matrix files

---
+++ LargeMatrixCalculations/iterativeSolvers.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def readMatrixFile(file, matrix_size):
+    f = open(file)
+    M = [[-1 for i in range(matrix_size)] for j in range(matrix_size)]
+    for line in f:
+        
+    return M
+
+def jacobi():
+    return
+
+def guassSeidel():
+    return
+
+def SuccesiveOverRelaxation():
+    w = 1.05
+
+def main():
+    A1 = readMatrixFile("/A1.matrix")
