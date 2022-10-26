fdbe81ac4f6e6ed2de0e1f82ad7fff8339e53414
iPupkin <blakeyvd@gmail.com>
2022-10-26 11:56:18 -0600

    started iterative solvers

--- LargeMatrixCalculations/iterativeSolvers.py
+++ LargeMatrixCalculations/iterativeSolvers.py
@@ -2,9 +2,10 @@ import numpy as np
 
 def readMatrixFile(file, matrix_size):
     f = open(file)
-    M = [[-1 for i in range(matrix_size)] for j in range(matrix_size)]
+    M = [[0 for i in range(matrix_size)] for j in range(matrix_size)]
     for line in f:
-        
+        values = int(line.split(" "))
+        M[values[0]][values[1]] = values[2]
     return M
 
 def jacobi():
@@ -17,4 +18,7 @@ def SuccesiveOverRelaxation():
     w = 1.05
 
 def main():
-    A1 = readMatrixFile("/A1.matrix")
+    A1 = readMatrixFile("A1.matrix", 793)
+    print(A1[0])
+    
+main()
