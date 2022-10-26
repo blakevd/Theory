51f126ea05920060c2c24ec2b155104e8f77c781
iPupkin <blakeyvd@gmail.com>
2022-10-26 12:46:27 -0600

    jacobi method

--- LargeMatrixCalculations/iterativeSolvers.py
+++ LargeMatrixCalculations/iterativeSolvers.py
@@ -1,15 +1,21 @@
 import numpy as np
 
+# reads in sparse matrix and stores as a 2D array
 def readMatrixFile(file, matrix_size):
     f = open(file)
-    M = [[0 for i in range(matrix_size)] for j in range(matrix_size)]
+    M = [[0 for i in range(matrix_size+1)] for j in range(matrix_size+1)]
     for line in f:
-        values = int(line.split(" "))
-        M[values[0]][values[1]] = values[2]
+        values = line.split(" ")
+        M[int(values[0])][int(values[1])] = float(values[2])
     return M
 
-def jacobi():
-    return
+# solves Ax = b jacobis method
+def jacobi(A, b, n):
+    Diagonal = np.diag(A)
+    Left = A - np.diagflat(Diagonal)
+    
+    for _ in range():
+        return
 
 def guassSeidel():
     return
@@ -18,7 +24,7 @@ def SuccesiveOverRelaxation():
     w = 1.05
 
 def main():
-    A1 = readMatrixFile("A1.matrix", 793)
-    print(A1[0])
+    M1 = readMatrixFile("A1.matrix", 793)
+    
     
 main()
