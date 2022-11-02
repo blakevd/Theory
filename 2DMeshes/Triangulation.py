decb889cd5641916c518467e17f64d41597077d0
iPupkin <blakeyvd@gmail.com>
2022-11-01 21:08:01 -0600

    started adding random points

--- 2DMeshes/Triangulation.py
+++ 2DMeshes/Triangulation.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from scipy.spatial import Delaunay
 import numpy as np
+import random
 
 # reads in node files
 def input(name):
@@ -11,16 +12,25 @@ def input(name):
         i = line.replace('\t', ' ').replace('\n', ' ').strip().split(' ')
         while '' in i:
             i.remove('')
-
         if i[0] != '#':
             points.append( [ float(i[1]), float(i[2]) ] )
             
-            
     return points
 
-def main():
-    points = np.array(input('circle2d-outer.node'))
+def addRandomPoints(points):
+    p = points.copy()
+    for i in range(250):
+        x = random.uniform(-1, 1)
+        y = random.uniform(-1, 1)
+        p = np.append(points, np.array([x, y]))
+    
+    return p
 
+def main():
+    circle = np.array(input('circle2d-outer.node'))
+    points = addRandomPoints(circle)
+    
+    print(len(circle), len(points))
     tri = Delaunay(points)
     plt.triplot(points[:,0], points[:,1], tri.simplices)
     plt.plot(points[:,0], points[:,1], 'o')
